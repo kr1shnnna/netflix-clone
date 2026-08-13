a9b595b34bd5f33ff3f2feadb858a572a92c7f02
Krishna <krishna9842740994@gmail.com>
2026-08-13 19:16:17 +0530

    titlecards half-done

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,41 @@
 import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+import { useRef } from 'react'
+import { useEffect } from 'react'
+
+
 const TitleCards = () => {
+
+ const cardsRef=useRef();
+
+ const handleWheel=(e)=>{
+  event.preventDefault();
+  cardsRef.current.scrollLeft+=e.deltaY;
+  
+ }
+
+ useEffect(()=>{
+cardsRef.current.addEventListener('wheel',handleWheel);
+ },[])
+
   return (
-    <div className='titlecards'>TitleCards</div>
+    <div className='title-cards'>
+
+      <h2>
+        Popular on Netflix </h2>
+        <div className="card-list" ref={cardsRef}>
+          {cards_data.map((card,index)=>{
+            return <div className="card" key={index}>
+              
+              <img src={card.image} />
+              <p>{card.name}</p>
+
+            </div>
+
+
+          })}
+        </div>
+      </div>
   )
 }
 
